ef946260b9e7408498d1895f3362a59acefef6af
Eric Booker <eric90booker@gmail.com>
2019-06-08 14:10:48 -0500

    added radio buttons

--- ZipInfo.js
+++ ZipInfo.js
@@ -9,6 +9,16 @@ class ZipInfo{
         this.tempLow = tempLow;
         this.countryName = countryName;
     }
+
+    toFahrenheit(){
+        this.tempHigh = tempHigh  * (9/5) - 459.67;
+        this.tempLow = this.tempLow * (9/5) - 459.67;
+    }
+
+    toCelsius(){
+        this.tempHigh = this.tempHigh * (9/5) - 459.67;
+        this.tempLow = this.tempHigh * (9/5) - 459.67;
+    }
 }
 
 
