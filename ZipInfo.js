8e1a7092fb91e4524f5af0f51df04b8707fea017
Eric Booker <eric90booker@gmail.com>
2019-06-07 19:17:01 -0500

    first commit

---
+++ ZipInfo.js
@@ -0,0 +1,12 @@
+
+class ZipInfo{
+    constructor(humidity, pressure, temperature, tempHigh, tempLow, countryName){
+        this.humidity = humidity;
+        this.pressure = pressure;
+        this.temperature = temperature;
+        this.tempHigh = tempHigh;
+        this.tempLow = tempLow;
+        this.countryName = countryName;
+    }
+}
+
---
+++ app.js
@@ -0,0 +1,24 @@
+zipStorage = window.localStorage;
+
+function getJSONData() {
+    const apiKey = "668a6cac8ea02dacb1cafff04454843c"
+    var jsonObject;
+    var zipCode = document.getElementById("zipInput").value;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            jsonObject = JSON.parse(this.responseText);
+            storeObject(jsonObject, zipCode);
+        }
+    }
+    xhr.open("GET", `http://api.openweathermap.org/data/2.5/weather?zip=${zipCode}` + 
+    "&appid=" + apiKey, true);
+    xhr.send();
+}
+
+function storeObject(jsonObject, zipCode){
+    var jsonObject = jsonObject;
+    var zipObject = new ZipInfo(jsonObject.main.humidity, jsonObject.main.pressure, jsonObject.main.temp,
+        jsonObject.main.temp_max, jsonObject.main.temp_min, jsonObject.sys.country);
+    console.log(zipObject);
+}
